259cd17c286e48e87598cb122fefe7842d415b07
zo <77091655+ZozonTeq@users.noreply.github.com>
2022-01-14 17:41:09 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,18 @@
 words_list = dict()
+banW = "1234567890!<>().,:;-!?'"
 
-inp = input()
-for i in inp.split(' '):
-    i=i.replace('.','').lower()
-    if i not in words_list.keys():
-        words_list[i] =1
-    else:
-        words_list[i] +=1
-    
-print(words_list)
-    
-
+while True:
+        print("---------------------")
+        inp = input()
+        for i in inp.split(' '):
+                i=i.lower()
+                for j in range(len(banW)):
+                    i = i.replace(banW[j],"")
+                if i == "": continue
+                if i.isascii() is True:
+                    if i not in words_list.keys():
+                        words_list[i] =1
+                    else:
+                        words_list[i] +=1
+        sorted_words = sorted(words_list.items(), key=lambda x:x[1])
+        print(sorted_words)
